ec58c817e9a681393ef4df9cd5bb4587d72886fd
Preston Fitzgerald <prestonfitz@gmail.com>
2024-10-10 20:24:43 -0600

    Save changes (in case I need to revert)

---
+++ donuts/Data/DonutDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace donuts.Data
+{
+    public class DonutDbContext : DbContext
+    {
+        public DonutDbContext(DbContextOptions<DonutDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Donuts> Donuts{ get; set; }
+    }
+}
---
+++ donuts/Migrations/20241010012149_init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace donuts.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Donuts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    flavor = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    variety = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    image = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    price = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donuts", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donuts");
+        }
+    }
+}
